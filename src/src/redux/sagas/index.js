962fd0c1da9efbd072c09a7acad11e64279d2b3a
saeidhy <134721370+saeidhy@users.noreply.github.com>
2025-01-14 13:29:22 +0330

    Delete src/src directory

--- src/src/redux/sagas/index.js
+++
@@ -1,8 +0,0 @@
-// src/redux/sagas/index.js
-
-import { all } from 'redux-saga/effects';
-import { watchFetchProducts } from './productSaga';
-
-export default function* rootSaga() {
-  yield all([watchFetchProducts()]);
-}
